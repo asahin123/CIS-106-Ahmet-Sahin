26a3ed9c77e55ff643b2a6c56bac9b7265ffe38d
asahin123 <111587760+asahin123@users.noreply.github.com>
2022-09-01 14:47:35 -0500

    Update Assignment 2.py

--- Assignment 2.py
+++ Assignment 2.py
@@ -1,1 +1,20 @@
-
+<?xml version="1.0"?>
+<flowgorithm fileversion="3.0">
+    <attributes>
+        <attribute name="name" value=""/>
+        <attribute name="authors" value="i am a gamer bitch"/>
+        <attribute name="about" value=""/>
+        <attribute name="saved" value="2022-09-01 12:07:42 PM"/>
+        <attribute name="created" value="aSBhbSBhIGdhbWVyIGJpdGNoO0RFU0tUT1AtTjYzSThQTjsyMDIyLTA4LTMxOzA5OjUyOjEzIFBNOzM5MTc="/>
+        <attribute name="edited" value="aSBhbSBhIGdhbWVyIGJpdGNoO0RFU0tUT1AtTjYzSThQTjsyMDIyLTA5LTAxOzEyOjAwOjE0IFBNOzc7NDAxNw=="/>
+        <attribute name="edited" value="QWhtZXQgSGFsaWQgU2FoaW47REVTS1RPUC1ONjNJOFBOOzIwMjItMDktMDE7MTI6MDc6NDIgUE07NDszOTgw"/>
+    </attributes>
+    <function name="Main" type="None" variable="">
+        <parameters/>
+        <body>
+            <declare name="Name" type="String" array="False" size=""/>
+            <assign variable="name" expression="&quot;Ahmet!&quot;"/>
+            <output expression="&quot;Hello  &quot; &amp; name" newline="True"/>
+        </body>
+    </function>
+</flowgorithm>
