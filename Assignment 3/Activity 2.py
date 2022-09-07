0e19c38aac1d7a0e63ccba2a481ac9c1010384f0
asahin123 <111587760+asahin123@users.noreply.github.com>
2022-09-07 00:22:45 -0500

    Update Activity 2.py

--- Assignment 3/Activity 2.py
+++ Assignment 3/Activity 2.py
@@ -1,11 +1,10 @@
-
-# This program shows a 24 year old in years,months,days,hours, and seconds.
-years = 24
+# This program shows an expected age in years,months,days,hours, and seconds.
+age = int(input())
+years = age
 months = 12
 days = 365
 hours = 8765.81277
 seconds = 31556926
-print("age=24")
 print("years=" + str(years))
 print("months=" + str(years * months))
 print("days=" + str(years * days))
