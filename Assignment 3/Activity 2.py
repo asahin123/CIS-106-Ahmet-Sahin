8f4f2b3ba2d7295c456d07872a4e8ebbc84bc4fd
asahin123 <111587760+asahin123@users.noreply.github.com>
2022-09-07 00:38:42 -0500

    Update Activity 2.py

--- Assignment 3/Activity 2.py
+++ Assignment 3/Activity 2.py
@@ -3,13 +3,12 @@ age = int(input())
 years = age
 months = 12
 days = 365
-hours = 8765.81277
-seconds = 31556926
+hours = 8760
+seconds = 31536000
 print("years=" + str(years))
 print("months=" + str(years * months))
 print("days=" + str(years * days))
 print("hours=" + str(years * hours))
 print("seconds=" + str(years * seconds))
 
-
-## References
+# References:https://www.youtube.com/watch?v=ogT550rGPiY
