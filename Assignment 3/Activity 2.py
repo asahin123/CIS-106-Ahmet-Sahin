7628234ed54d4d511eafdda1f273d18cde4a044e
asahin123 <111587760+asahin123@users.noreply.github.com>
2022-09-06 23:57:30 -0500

    Update Activity 2.py

--- Assignment 3/Activity 2.py
+++ Assignment 3/Activity 2.py
@@ -1,1 +1,13 @@
 
+# This program shows a 24 year old in years,months,days,hours, and seconds.
+years = 24
+months = 12
+days = 365
+hours = 8765.81277
+seconds = 31556926
+print("age=24")
+print("years=" + str(years))
+print("months=" + str(years * months))
+print("days=" + str(years * days))
+print("hours=" + str(years * days))
+print("seconds=" + str(years * seconds))
