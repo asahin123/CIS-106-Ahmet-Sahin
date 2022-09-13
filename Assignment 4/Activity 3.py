f21bd1051a59be511cb3decc80c3d1624cdb44ac
asahin123 <111587760+asahin123@users.noreply.github.com>
2022-09-13 16:59:41 -0500

    Create Activity 3.py

---
+++ Assignment 4/Activity 3.py
@@ -0,0 +1,9 @@
+# This program shows how to calculate the expected distance in kilometers, meters, and centimeters.
+miles = int(input())
+distance = miles
+kilometers = miles * 1.609344
+meters = miles * 1609.344
+centimeters = miles * 160934.4
+print(" kilometers=" + str(kilometers))
+print(" meters=" + str(meters))
+print(" centimeters=" + str(centimeters))
