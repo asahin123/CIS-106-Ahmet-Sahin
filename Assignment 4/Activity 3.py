ee624ba7452bdf7377c532b9fa05a467d9c7b182
Dave Braunschweig <dave@itrain.ms>
2022-09-16 08:36:26 -0500

    Assignment corrections

--- Assignment 4/Activity 3.py
+++ Assignment 4/Activity 3.py
@@ -1,14 +1,16 @@
-# This program shows how to calculate the expected distance in milesi into kilometers, meters, and centimeters.
+# This program shows how to calculate the expected distance in miles
+# into kilometers, meters, and centimeters.
 
+# References: 
+#   For this session, I took help from my friend who also does coding.
+
+print("Enter miles:")
 miles = int(input())
 
-distance = miles
 kilometers = miles * 1.609344
 meters = miles * 1609.344
 centimeters = miles * 160934.4
 
-print(" kilometers=" + str(kilometers))
-print(" meters=" + str(meters))
-print(" centimeters=" + str(centimeters))
-
-# References: For this session, I took help from my friend who also does coding.
+print("kilometers=" + str(kilometers))
+print("meters=" + str(meters))
+print("centimeters=" + str(centimeters))
