9edf573b5cc486f6b34f37e61a898fb0a772c797
asahin123 <111587760+asahin123@users.noreply.github.com>
2022-09-26 22:23:42 -0500

    Update Activity 2.py

--- Assignment 6/Activity 2.py
+++ Assignment 6/Activity 2.py
@@ -18,8 +18,13 @@ def calculateSeconds(age):
     
     return seconds
 
-def displayResult(age, months, days, hours, seconds):
-    print(str(age) + "Age is " + str(months) + "Months " + str(days) + "Days " + str(hours) + "Hours " + str(seconds) + "Seconds ")
+def calculateYears(age):
+    years = age
+    
+    return years
+
+def displayResult(months, days, hours, seconds):
+    print(str(months) + " Months " + str(days) + " Days " + str(hours) + " Hours " + str(seconds) + " Seconds ")
 
 def getAge():
     print("Enter Age")
@@ -30,10 +35,11 @@ def getAge():
 # Main
 # This program shows an expected age in months, days, hours, and seconds.
 age = getAge()
+years = calculateYears(age)
 months = calculateMonths(age)
 days = calculateDays(age)
 hours = calculateHours(age)
 seconds = calculateSeconds(age)
-displayResult(age, months, days, hours, seconds)
+displayResult(months, days, hours, seconds)
 
 # References: https://www.youtube.com/watch?v=FWaN3lTyhPU
