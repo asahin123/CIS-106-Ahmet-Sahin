ce681c5ee3e81afc6e861d5215c77b9fe5b30b5d
asahin123 <111587760+asahin123@users.noreply.github.com>
2022-10-10 18:18:30 -0500

    Update Activity 4.py

--- Assignment 8/Activity 4.py
+++ Assignment 8/Activity 4.py
@@ -2,17 +2,29 @@
 
 # References:https://www.youtube.com/watch?v=TeFiW-2kvTY
 
-print("Enter an iteration number")
-iterationNumber = int(input())
+def forLoop(iterationNumber, fibonacci):
+    for i in range(0, iterationNumber + 1 + 1, 1):
+        print(fibonacci[i])
+
+def getIterationNumber():
+    print("Enter an iteration number")
+    iterationNumber = int(input())
+    
+    return iterationNumber
+
+def whileLoop(iterationNumber, fibonacci):
+    count = 0
+    while count < iterationNumber:
+        fibonacci[count + 2] = fibonacci[count + 1] + fibonacci[count]
+        count = count + 1
+
+# Main
+iterationNumber = getIterationNumber()
 fibonacci = [0] * (iterationNumber + 2)
 
 fibonacci[0] = 0
 fibonacci[1] = 1
-count = 0
-while count < iterationNumber:
-    fibonacci[count + 2] = fibonacci[count + 1] + fibonacci[count]
-    count = count + 1
-for i in range(0, iterationNumber + 1 + 1, 1):
-    print(fibonacci[i])
+whileLoop(iterationNumber, fibonacci)
+forLoop(iterationNumber, fibonacci)
 
-# References:https://www.youtube.com/watch?v=TeFiW-2kvTY
+
