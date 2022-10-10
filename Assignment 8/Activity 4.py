db80490773145cddd5da4bf8d2c5c6cbc5f7be97
asahin123 <111587760+asahin123@users.noreply.github.com>
2022-10-10 17:39:24 -0500

    Create Activity 4.py

---
+++ Assignment 8/Activity 4.py
@@ -0,0 +1,19 @@
+# This program demonstrates while loop counting using the Fibonacci sequence based on a given number of iterations entered by the user.
+
+# References:https://www.youtube.com/watch?v=TeFiW-2kvTY
+
+print("Enter an iteration number")
+iterationNumber = int(input())
+fibonacci = [0] * (iterationNumber + 2)
+
+fibonacci[0] = 0
+fibonacci[1] = 1
+count = 0
+print(fibonacci[count])
+print(fibonacci[count + 1])
+while count < iterationNumber:
+    fibonacci[count + 2] = fibonacci[count + 1] + fibonacci[count]
+    print(fibonacci[count + 2])
+    count = count + 1
+for i in range(0, iterationNumber + 1 + 1, 1):
+    print(fibonacci[i])
