fd581a22dcf9790d586c0ebdfad2610d824045e4
asahin123 <111587760+asahin123@users.noreply.github.com>
2022-10-26 16:21:35 -0500

    Update Activity 2.py

--- Assignment 10/Activity 2.py
+++ Assignment 10/Activity 2.py
@@ -3,7 +3,7 @@
 # References:https://www.youtube.com/watch?v=TjkJQly2YCw
 
 def displayCount(count):
-    print("After " + str(count) + " guesses, the number in your mind is found")
+    print("" + str(count) + " guesses, guess, , guess")
 
 def displayGameIntroduction():
     print("Please, think of a number between 0 and 100, inclusive.")
