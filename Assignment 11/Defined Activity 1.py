d61bf660c7e2cfe41d7cd59a56efad08fe1b8a3b
asahin123 <111587760+asahin123@users.noreply.github.com>
2022-11-03 21:10:05 -0500

    Update Defined Activity 1.py

--- Assignment 11/Defined Activity 1.py
+++ Assignment 11/Defined Activity 1.py
@@ -99,5 +99,5 @@ def main():
             display_finish()    
     else:
         display_finish()
-    if not (value_year > 0 and value_month >= 1 and value_month <= 12
+    if not (value_year > 0 and value_month >= 1 and value_month <= 12)
 main()
