62f8464307643b160ed901c86398bceae5654f77
asahin123 <111587760+asahin123@users.noreply.github.com>
2022-11-15 17:59:41 -0600

    Create Activity 1.py

---
+++ Assignment 13/Activity 1.py
@@ -0,0 +1,25 @@
+#This program asks the user for a single line of text containing a first name 
+#and last name and print out the name in the form last name, first initial, such as Lastname, F.
+
+#https://www.w3schools.com/python/python_strings.asp
+
+def get_full_name():
+    fullname = input("Enter Full Name : ")
+    return fullname
+
+def process_full_name(fullname):
+    name_list = fullname.split()
+    name = name_list[-1].title() + ", " + name_list[0][0:1].title() + "."
+    return name
+
+def display_full_name(processed_name):
+    print(processed_name)
+    return
+
+
+def main():
+    fullname = get_full_name()
+    processed_name = process_full_name(fullname)
+    display_full_name(processed_name)
+
+main()
