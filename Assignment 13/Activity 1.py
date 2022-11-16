58d498d5d1b7a2f5152e2f2dd98f8e5d1333a80d
asahin123 <111587760+asahin123@users.noreply.github.com>
2022-11-16 01:21:01 -0600

    Update Activity 1.py

--- Assignment 13/Activity 1.py
+++ Assignment 13/Activity 1.py
@@ -5,7 +5,7 @@
 # References: https://www.w3schools.com/python/python_strings.asp
 
 def get_full_name():
-    fullname = input(" Firstname Lastname : ")
+    fullname = input(" Please enter your name : ")
     return fullname
 
 
