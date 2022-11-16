2ae48d76b137879c7fcdb79b8a8ae10f757e218c
asahin123 <111587760+asahin123@users.noreply.github.com>
2022-11-16 12:27:33 -0600

    Update Activity 1.py

--- Assignment 13/Activity 1.py
+++ Assignment 13/Activity 1.py
@@ -5,25 +5,24 @@
 # References: https://www.w3schools.com/python/python_strings.asp
 
 def get_full_name():
-    fullname = input(" Please enter your name : ")
+    fullname = input("Enter Full Name : ")
     return fullname
 
-
 def process_full_name(fullname):
-    name_list = fullname.split()
-    name = name_list[-1].title() + ", " + name_list[0][0:1].title() + "."
+    if len(fullname) == 0 or fullname.isspace() or len(fullname.split()) == 1:
+        name = "Wrong Index, Please enter a name"
+    else:
+        name_list = fullname.split()
+        name = name_list[-1].title() + ", " + name_list[0][0:1].title() + "."
     return name
-
 
 def display_full_name(processed_name):
     print(processed_name)
     return
 
-
 def main():
     fullname = get_full_name()
     processed_name = process_full_name(fullname)
     display_full_name(processed_name)
-    
 
 main()
