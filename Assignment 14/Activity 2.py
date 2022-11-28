9ccb786662ea21af6c99cc4df82f4fdc1c2f6588
asahin123 <111587760+asahin123@users.noreply.github.com>
2022-11-28 16:13:37 -0600

    Create Activity 2.py

---
+++ Assignment 14/Activity 2.py
@@ -0,0 +1,43 @@
+def display_result(list_scores, max, min, ave):
+    print()
+    print("Score List : ")
+    print(list_scores)
+    print()
+    print("Maximum Score : {0:.2f}".format(max))
+    print("Minimum Score : {0:.2f}".format(min))
+    print("Average Score : {0:.2f}".format(ave))
+
+
+def read_file(filename):
+    try:
+        with open(filename, "r") as file:
+            list_scores = []
+            for line in file:
+                lines = line.strip().split(",") # Name,Score  ==>  ["Name","Score"]
+                line1 = lines[1]
+                if line1.isnumeric():
+                    list_scores.append(int(lines[1]))
+            return list_scores
+    except Exception as exception:
+        print(exception)
+
+
+def file_process(list_scores):
+    summation = 0
+    for score in list_scores:
+        summation = summation + int(score)
+    max_value = max(list_scores)
+    min_value = min(list_scores)
+    average = summation / len(list_scores)
+    display_result(list_scores, max_value, min_value, average)
+
+
+def main():
+    filename = "scores.txt"
+    list_scores = read_file(filename)
+    file_process(list_scores)
+
+
+main()
+
+
