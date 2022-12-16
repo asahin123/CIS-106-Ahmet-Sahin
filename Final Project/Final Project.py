8e8fd91259448bd912e65f4b7f889439f2edd63c
Dave Braunschweig <dave@itrain.ms>
2022-12-16 13:21:57 -0600

    Final Project structure

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -5,30 +5,56 @@
 # References: https://youtu.be/pTB0EiLXUC8
 
 
-def display_result(number_of_items, total_price):
+def list_plants(filename, tag):
+    import re
+
+    array = []
     try:
+        with open(filename, "r") as file:
+            for line in file:
+                if re.search(tag, line):
+                    array.append(line[line.find(">") + 1:line.find("/") - 1])
+        return array
+    except FileNotFoundError:
+        print("File is missing")
+        exit()
+
+
+def process_calculation(price):
+    total_price = 0
+    try:
+        for x in range(len(price)):
+            total_price = total_price + float(price[x].lstrip("$"))
+
+        average_price = total_price / len(price)
+
+        return average_price
+    except IndexError:
         print()
-        text = "{0} items - ${1:.2f} average price"
-        print(text.format(number_of_items, total_price / number_of_items))
-    except ZeroDivisionError:
-        print("Division by Zero because of the variable is zero")
+        print("Error: Missing or bad data.")
         raise SystemExit
+    except ZeroDivisionError:
+        print("Error: Missing or bad data.")
+        raise SystemExit
+    except ValueError:
+        print("Error: Missing or bad data.")
+        raise SystemExit
+    except TypeError as e:
+        print(e)
+    except FileNotFoundError:
+        print("File is missing")
 
 
 def display_plant(common, botanical, zone, light, price):
-    text = "{0} ({1}) - {2} - {3} - {4}"
-    print(text.format(common, botanical, zone, light, price))
-
-
-def process_calculation(common, botanical, zone, light, price):
-    total_price = 0
-    number_of_items = 0
     try:
-        for x in range(len(common)):
-            display_plant(common[x], botanical[x], zone[x], light[x], price[x])
-            total_price = total_price + float(price[x].lstrip("$"))
-            number_of_items = number_of_items + 1
-        display_result(number_of_items, total_price)
+        for index in range(len(common)):
+            text = "{0} ({1}) - {2} - {3} - {4}"
+            print(text.format(
+                common[index], 
+                botanical[index], 
+                zone[index], 
+                light[index], 
+                price[index]))
     except IndexError:
         print()
         print("Error: Missing or bad data.")
@@ -42,50 +68,32 @@ def process_calculation(common, botanical, zone, light, price):
         print("File is missing")
 
 
-def list_plants(filename):
-    import re
+def display_result(number_of_items, average_price):
+    if number_of_items == 0:
+        print("File is empty, missing, or has bad data")
 
-    common = []
-    botan = []
-    zone = []
-    light = []
-    price = []
     try:
-        with open(filename, "r") as file:
-            for line in file:
-                if re.search("COMMON", line):
-                    common.append(line[line.find(">") + 1:line.find("/") - 1])
-                if re.search("BOTANICAL", line):
-                    botan.append(line[line.find(">") + 1:line.find("/") - 1])
-                if re.search("ZONE", line):
-                    zone.append(line[line.find(">") + 1:line.find("/") - 1])
-                if re.search("LIGHT", line):
-                    light.append(line[line.find(">") + 1:line.find("/") - 1])
-                if re.search("PRICE", line):
-                    price.append(line[line.find(">") + 1:line.find("/") - 1])
-    except FileNotFoundError:
-        print("File is missing")
-    else:
-        process_calculation(common, botan, zone, light, price)
-
-
-def load_xml(filename):
-    import urllib.request
-
-    url = "https://www.w3schools.com/xml/plant_catalog.xml"
-    try:
-        page = urllib.request.urlopen(url).read().decode("UTF-8")
-        with open(filename, 'w') as file_name:
-            file_name.write(str(page))
-    except Exception as exception:
-        print(str(exception) + " reading " + url)
+        print()
+        text = "{0} items - ${1:.2f} average price"
+        print(text.format(number_of_items, average_price))
+    except ZeroDivisionError:
+        print("Division by Zero because of the variable is zero")
         raise SystemExit
 
 
 def main():
     filename = 'plant_catalog.xml'
-    load_xml(filename)
-    list_plants(filename)
+    common = list_plants(filename, "COMMON")
+    botanical = list_plants(filename, "BOTANICAL")
+    zone = list_plants(filename, "ZONE")
+    light = list_plants(filename, "LIGHT")
+    price = list_plants(filename, "PRICE")
+
+    number_of_items = len(price)
+    average_price = process_calculation(price)
+
+    display_plant(common, botanical, zone, light, price)
+    display_result(number_of_items, average_price)
 
 
 main()
